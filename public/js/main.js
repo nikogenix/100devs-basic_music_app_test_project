9868dd8900e4aea3d511e4b1a3bfd80dc64f1d36
justKara <nikogenix@gmail.com>
2022-11-25 13:41:12 +0200

    Add edit functionality. Change table headers

--- public/js/main.js
+++ public/js/main.js
@@ -29,4 +29,69 @@ async function deleteItem() {
 	}
 }
 
-async function editItem() {}
+async function editItem() {
+	const itemArtistNode = this.previousElementSibling.previousElementSibling;
+	const itemArtist = itemArtistNode.innerText;
+	const itemSongNode = this.previousElementSibling;
+	const itemSong = itemSongNode.innerText;
+	const cancelButton = this.nextElementSibling;
+
+	// Update DOM
+
+	this.classList.remove("fa-edit");
+	this.classList.add("fa-save");
+
+	cancelButton.classList.remove("fa-trash");
+	cancelButton.classList.add("fa-times");
+
+	itemArtistNode.innerText = "";
+	itemSongNode.innerText = "";
+
+	let inputArtistNode = document.createElement("input");
+	inputArtistNode.setAttribute("type", "text");
+	inputArtistNode.setAttribute("name", "artist");
+	inputArtistNode.setAttribute("required", "");
+	inputArtistNode.value = itemArtist;
+
+	let inputSongNode = document.createElement("input");
+	inputSongNode.setAttribute("type", "text");
+	inputSongNode.setAttribute("name", "song");
+	inputSongNode.setAttribute("required", "");
+	inputSongNode.value = itemSong;
+
+	itemArtistNode.appendChild(inputArtistNode);
+	itemSongNode.appendChild(inputSongNode);
+
+	// Save
+	this.addEventListener("click", async () => {
+		const inputArtist = inputArtistNode.value;
+		const inputSong = inputSongNode.value;
+
+		try {
+			const response = await fetch("editMusic", {
+				method: "put",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({
+					artistOld: itemArtist,
+					songOld: itemSong,
+					artistNew: inputArtist,
+					songNew: inputSong,
+				}),
+			});
+			const data = await response.json();
+			console.log(data);
+			location.reload();
+		} catch (err) {
+			console.log(err);
+		}
+	});
+
+	// Cancel
+	cancelButton.addEventListener("click", () => {
+		try {
+			location.reload();
+		} catch (err) {
+			console.log(err);
+		}
+	});
+}
